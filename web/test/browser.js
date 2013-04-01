93f809940ce4eb52c8cb5160ebb92af647de248b
Kenneth Powers <mail@kenpowers.net>
2013-04-01 01:34:41 -0400

    Set up testing framework.

---
+++ web/test/browser.js
@@ -0,0 +1,14 @@
+/*global describe it chai*/
+describe('GEA Browser Tests', function () {
+  var expect = chai.expect;
+  describe('unit tests', function () {
+    it('should run', function () {
+      expect(true).to.be.true;
+    });
+  });
+  describe('integration tests', function () {
+    it('should run', function () {
+      expect(true).to.be.true;
+    });
+  });
+});
---
+++ web/test/server.js
@@ -0,0 +1,7 @@
+/*global describe it require:true*/
+describe('GEA Server Tests', function () {
+  var expect = require('chai').expect;
+  it('should run', function () {
+    expect(true).to.be.true;
+  });
+});
